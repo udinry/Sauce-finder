09eb0c2a2f69f68b80ad4256ca41b2c1373f795d
Udbhav <udbhav.naryani@gmail.com>
2021-04-16 12:43:35 +0530

    Connected MongoDB

--- Server/app.js
+++ Server/app.js
@@ -1,23 +1,34 @@
 const express = require('express')
 const app = express()
 const PORT = 5000
-
-const customMiddleware = (req, res, next)=>{
-    console.log("middleware executed!!")
-    next()
-}
-
-//app.use(customMiddleware)
-
-app.get('/',(req,res)=>{
-    console.log("home")
-    res.send("Hello World !")
+const mongoose = require('mongoose')
+const {MONGOURI} = require('./keys')
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
 })
-
-app.get('/about',customMiddleware,(req,res)=>{
-    console.log("about")
-    res.send("about page")
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongoDB")
 })
+mongoose.connection.on('error',(err)=>{
+    console.log("error in connection",err)
+})
+// const customMiddleware = (req, res, next)=>{
+//     console.log("middleware executed!!")
+//     next()
+// }
+
+// //app.use(customMiddleware)
+
+// app.get('/',(req,res)=>{
+//     console.log("home")
+//     res.send("Hello World !")
+// })
+
+// app.get('/about',customMiddleware,(req,res)=>{
+//     console.log("about")
+//     res.send("about page")
+// })
 
 app.listen(PORT, ()=>{
     console.log("server is running on", PORT)
