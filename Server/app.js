1d10e20f00d1cddea7753d2eb7abcbbe43906625
Udbhav Naryani <udbhav.naryani@gmail.com>
2021-04-20 10:34:22 +0530

    modified app.js to test GET using POSTMAN

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,10 @@ const app = express()
 const PORT = 5000
 const mongoose = require('mongoose')
 const {MONGOURI} = require('./keys')
+require('./models/user')
+mongoose.model("User")
+app.use(express.json())
+app.use(require('./routes/auth'))
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
     useUnifiedTopology:true
